dbb818eaf976d2e8c9ef67c16081ddd3f6e31399
ranasurajit <surajit1602@gmail.com>
2026-01-29 01:47:06 +0530

    feat[chapter03][topic02]: component ecosystem of react > building ui cards with tailwind

---
+++ Chapter_03_Component_Ecosystem_Of_React/Sub_02_Building_UI_Cards_With_Tailwind/src/App.jsx
@@ -0,0 +1,70 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className='text-blue-600 dark:text-sky-400 border-2 p-4 rounded-2xl'>
+        React Build UI Cards With Tailwind
+      </h1>
+      {/* Template for Card UI */}
+      <div className='flex gap-3 flex-wrap justify-around'>
+        <div className='max-w-sm bg-white border border-gray-200 
+        rounded-xl mt-8 shadow overflow-hidden transition-shadow'>
+          <img className='w-full h-48 object-cover' src='https://images.pexels.com/photos/35742275/pexels-photo-35742275.jpeg' alt='Sample Image' />
+          <div className='p-4'>
+            <h2 className='text-lg font-semibold text-gray-800'>Card Title</h2>
+            <p className='mt-2 text-gray-600 text-sm'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis perferendis, tempora earum ratione fugit enim, sit iste sequi eum sed accusamus, eaque facilis cum. Sit exercitationem ea voluptas voluptates excepturi.</p>
+            <button className='mt-4 px-4 bg-blue-600 hover:bg-blue-400 rounded-lg text-white font-bold py-2'>Buy Now</button>
+          </div>
+        </div>
+        <div className='max-w-sm bg-white border border-gray-200 
+        rounded-xl mt-8 shadow overflow-hidden transition-shadow'>
+          <img className='w-full h-48 object-cover' src='https://images.pexels.com/photos/35742275/pexels-photo-35742275.jpeg' alt='Sample Image' />
+          <div className='p-4'>
+            <h2 className='text-lg font-semibold text-gray-800'>Card Title</h2>
+            <p className='mt-2 text-gray-600 text-sm'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis perferendis, tempora earum ratione fugit enim, sit iste sequi eum sed accusamus, eaque facilis cum. Sit exercitationem ea voluptas voluptates excepturi.</p>
+            <button className='mt-4 px-4 bg-blue-600 hover:bg-blue-400 rounded-lg text-white font-bold py-2'>Buy Now</button>
+          </div>
+        </div>
+        <div className='max-w-sm bg-white border border-gray-200 
+        rounded-xl mt-8 shadow overflow-hidden transition-shadow'>
+          <img className='w-full h-48 object-cover' src='https://images.pexels.com/photos/35742275/pexels-photo-35742275.jpeg' alt='Sample Image' />
+          <div className='p-4'>
+            <h2 className='text-lg font-semibold text-gray-800'>Card Title</h2>
+            <p className='mt-2 text-gray-600 text-sm'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis perferendis, tempora earum ratione fugit enim, sit iste sequi eum sed accusamus, eaque facilis cum. Sit exercitationem ea voluptas voluptates excepturi.</p>
+            <button className='mt-4 px-4 bg-blue-600 hover:bg-blue-400 rounded-lg text-white font-bold py-2'>Buy Now</button>
+          </div>
+        </div>
+        <div className='max-w-sm bg-white border border-gray-200 
+        rounded-xl mt-8 shadow overflow-hidden transition-shadow'>
+          <img className='w-full h-48 object-cover' src='https://images.pexels.com/photos/35742275/pexels-photo-35742275.jpeg' alt='Sample Image' />
+          <div className='p-4'>
+            <h2 className='text-lg font-semibold text-gray-800'>Card Title</h2>
+            <p className='mt-2 text-gray-600 text-sm'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis perferendis, tempora earum ratione fugit enim, sit iste sequi eum sed accusamus, eaque facilis cum. Sit exercitationem ea voluptas voluptates excepturi.</p>
+            <button className='mt-4 px-4 bg-blue-600 hover:bg-blue-400 rounded-lg text-white font-bold py-2'>Buy Now</button>
+          </div>
+        </div>
+        <div className='max-w-sm bg-white border border-gray-200 
+        rounded-xl mt-8 shadow overflow-hidden transition-shadow'>
+          <img className='w-full h-48 object-cover' src='https://images.pexels.com/photos/35742275/pexels-photo-35742275.jpeg' alt='Sample Image' />
+          <div className='p-4'>
+            <h2 className='text-lg font-semibold text-gray-800'>Card Title</h2>
+            <p className='mt-2 text-gray-600 text-sm'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis perferendis, tempora earum ratione fugit enim, sit iste sequi eum sed accusamus, eaque facilis cum. Sit exercitationem ea voluptas voluptates excepturi.</p>
+            <button className='mt-4 px-4 bg-blue-600 hover:bg-blue-400 rounded-lg text-white font-bold py-2'>Buy Now</button>
+          </div>
+        </div>
+        <div className='max-w-sm bg-white border border-gray-200 
+        rounded-xl mt-8 shadow overflow-hidden transition-shadow'>
+          <img className='w-full h-48 object-cover' src='https://images.pexels.com/photos/35742275/pexels-photo-35742275.jpeg' alt='Sample Image' />
+          <div className='p-4'>
+            <h2 className='text-lg font-semibold text-gray-800'>Card Title</h2>
+            <p className='mt-2 text-gray-600 text-sm'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis perferendis, tempora earum ratione fugit enim, sit iste sequi eum sed accusamus, eaque facilis cum. Sit exercitationem ea voluptas voluptates excepturi.</p>
+            <button className='mt-4 px-4 bg-blue-600 hover:bg-blue-400 rounded-lg text-white font-bold py-2'>Buy Now</button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App;
