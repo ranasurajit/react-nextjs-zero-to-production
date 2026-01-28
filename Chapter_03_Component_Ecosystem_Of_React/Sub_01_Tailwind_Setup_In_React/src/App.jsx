c59b4ec09fbd2349a197c18ea2161a6758926c32
ranasurajit <surajit1602@gmail.com>
2026-01-28 23:40:11 +0530

    feat[chapter03][topic01]: component ecosystem of react > tailwind setup in react

---
+++ Chapter_03_Component_Ecosystem_Of_React/Sub_01_Tailwind_Setup_In_React/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-lg text-blue-600 dark:text-blue-200 border-2 p-4 rounded-2xl'>
+        Tailwind setup on React Vite Project!
+      </h1>
+    </>
+  )
+}
+
+export default App;
